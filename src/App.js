c99893a8665b1af2c057fb4a8157401df67e9182
ADITYA SHARMA <124610598+Aadisharma111@users.noreply.github.com>
2023-06-05 12:38:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Page1 from './pages/Page1'
 import Page2 from './pages/Page2'
 
+//The Sync Async Function Both are Dependencies Array 
+// Value of Post And Pre-Mature Java scripted origin
+/*/ 
+// PORT-SET -- localhost//:3000 
+??*?? 
+//*Plz Confirm The Value*//
 
+// Alogoritham Should be Var char 
 
 function App() {
   return (
